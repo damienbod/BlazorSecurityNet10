e78dd2933172eb341258c36cec2ce9dec03cc7a2
damienbod <damien_bod@hotmail.com>
2025-05-25 08:50:28 +0200

    Update startup

--- BlazorApp/BlazorApp/Program.cs
+++ BlazorApp/BlazorApp/Program.cs
@@ -1,4 +1,10 @@
 using BlazorApp.Components;
+using BlazorApp;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,15 +12,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHttpContextAccessor();
+
+var oidcConfig = builder.Configuration.GetSection("OpenIDConnectSettings");
+
+builder.Services.AddSecurityHeaderPolicies()
+    .SetDefaultPolicy(SecurityHeadersDefinitions
+    .GetHeaderPolicyCollection(oidcConfig["Authority"],
+        builder.Environment.IsDevelopment()));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    IdentityModelEventSource.ShowPII = true;
+    IdentityModelEventSource.LogCompleteSecurityArtifact = true;
+}
+
+//app.UseSecurityHeaders();
+
 
 app.UseHttpsRedirection();
 
